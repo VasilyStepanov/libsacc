3636459c0d8c790b64904b565d0842232c2f7e45
Vasily Stepanov <vasily.stepanov@gmail.com>
2012-01-06 19:02:43 +0400

    Use indents in test_parser to control the parser's call graph.

--- src/test_parser.c
+++ src/test_parser.c
@@ -7,17 +7,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
+
+
+
+#define USERDATA_FILE(userData) ((struct userdata_t*)(userData))->file
+#define USERDATA_INDENT(userData) ((struct userdata_t*)(userData))->indent
+
+struct userdata_t {
+  FILE *file;
+  char *indent;
+};
+
+
+
+void* userdata_open(char **data, size_t *size) {
+  void *ret =
+    (struct userdata_t*)malloc(sizeof(struct userdata_t));
+
+  USERDATA_FILE(ret) = open_memstream(data, size);
+  USERDATA_INDENT(ret) = (char*)malloc(sizeof(char));
+  *USERDATA_INDENT(ret) = '\0';
+
+  return ret;
+}
+
+
+
+void userdata_close(void *userdata) {
+  free(USERDATA_INDENT(userdata));
+  fclose(USERDATA_FILE(userdata));
+  free(userdata);
+}
+
+
+
+void userdata_inc_indent(void *userdata) {
+  size_t len;
+  char *indent;
+
+  len = strlen(USERDATA_INDENT(userdata));
+  indent = realloc(USERDATA_INDENT(userdata), len + 1 + 2);
+  indent[len + 0] = ' ';
+  indent[len + 1] = ' ';
+  indent[len + 2] = '\0';
+  USERDATA_INDENT(userdata) = indent;
+}
+
+
+
+void userdata_dec_indent(void *userdata) {
+  size_t len;
+  char *indent;
+
+  len = strlen(USERDATA_INDENT(userdata));
+  indent = realloc(USERDATA_INDENT(userdata), len + 1 - 2);
+  indent[len - 2] = '\0';
+  USERDATA_INDENT(userdata) = indent;
+}
+
+
+
+void userdata_printf(void *userdata, const char *fmt, ...) {
+  va_list args;
+
+  va_start(args, fmt);
+  fprintf(USERDATA_FILE(userdata), "%s", USERDATA_INDENT(userdata));
+  vfprintf(USERDATA_FILE(userdata), fmt, args);
+}
 
 
 
 void start_document(void *userData) {
-  fprintf((FILE*)userData, "doc {\n");
+  userdata_printf(userData, "doc {\n");
+  userdata_inc_indent(userData);
 }
 
 
 
 void end_document(void *userData) {
-  fprintf((FILE*)userData, "doc }\n");
+  userdata_dec_indent(userData);
+  userdata_printf(userData, "doc }\n");
 }
 
 
@@ -28,30 +98,32 @@ void property(
   SAC_Boolean important)
 {
   if (important == SAC_FALSE) {
-    fprintf((FILE*)userData, "prp '%s'\n", propertyName);
+    userdata_printf(userData, "prp '%s'\n", propertyName);
   } else {
-    fprintf((FILE*)userData, "prp '%s' important\n", propertyName);
+    userdata_printf(userData, "prp '%s' important\n", propertyName);
   }
 }
 
 
 
 SAC_Parser create_parser(char **data, size_t *size) {
-  FILE *f = open_memstream(data, size);
-  SAC_Parser parser = SAC_CreateParser();
+  struct userdata_t *userData;
+  SAC_Parser parser;
+  
+  userData = userdata_open(data, size);
+
+  parser = SAC_CreateParser();
   
   SAC_SetDocumentHandler(parser, start_document, end_document);
   SAC_SetPropertyHandler(parser, property);
-  SAC_SetUserData(parser, f);
+  SAC_SetUserData(parser, userData);
   return parser;
 }
 
 
 
 void dispose_parser(SAC_Parser parser) {
-  FILE *f = SAC_GetUserData(parser);
-
-  fclose(f);
+  userdata_close(SAC_GetUserData(parser));
   SAC_DisposeParser(parser);
 }
 
@@ -78,7 +150,7 @@ void test_parser_basics() {
 
   assert_equals(
 "doc {\n"
-"prp 'property'\n"
+"  prp 'property'\n"
 "doc }\n",
   data);
 
